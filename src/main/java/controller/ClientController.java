cba61a494663b3121399c8b5ebf8a00ba810eefe
manithlakvidu2002 <lakvidu00@gmail.com>
2023-06-21 13:01:22 +0530

    Client Form Controller Created

--- src/main/java/controller/ClientController.java
+++ src/main/java/controller/ClientController.java
@@ -1,4 +1,69 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class ClientController {
+    public void txt_massage_on_action(ActionEvent actionEvent) {
+    }
+
+    public void Mouse_On_Click_Hide_Emogi(MouseEvent mouseEvent) {
+    }
+
+    public void btn_imogi_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void btn_cam_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void btn_massage_send_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void emo_pane_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void sad(MouseEvent mouseEvent) {
+    }
+
+    public void lot_sad(MouseEvent mouseEvent) {
+    }
+
+    public void money(MouseEvent mouseEvent) {
+    }
+
+    public void love(MouseEvent mouseEvent) {
+    }
+
+    public void green_sad(MouseEvent mouseEvent) {
+    }
+
+    public void smile_one_eyy(MouseEvent mouseEvent) {
+    }
+
+    public void cry_yes(MouseEvent mouseEvent) {
+    }
+
+    public void cry_head(MouseEvent mouseEvent) {
+    }
+
+    public void real_amile(MouseEvent mouseEvent) {
+    }
+
+    public void tuin(MouseEvent mouseEvent) {
+    }
+
+    public void woow(MouseEvent mouseEvent) {
+    }
+
+    public void smile_normal(MouseEvent mouseEvent) {
+    }
+
+    public void large_smile(MouseEvent mouseEvent) {
+    }
+
+    public void small_smile(MouseEvent mouseEvent) {
+    }
+
+    public void tong_smile(MouseEvent mouseEvent) {
+    }
 }
