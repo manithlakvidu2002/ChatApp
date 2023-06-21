e2f80ae257a870f1f7e2aba451362d427ed01ac7
manithlakvidu2002 <lakvidu00@gmail.com>
2023-06-21 14:29:27 +0530

    Loging Form Controller Update

--- src/main/java/controller/LogingFormController.java
+++ src/main/java/controller/LogingFormController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LogingFormController {
+
+    public JFXTextField Txt_Enter_Name;
+    static String User_name;
+
+    public void Txt_name_on_action(ActionEvent actionEvent) throws IOException {
+        User_name=Txt_Enter_Name.getText();
+        Txt_Enter_Name.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
